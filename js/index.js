51a2f73acfdbfcf77c74cdb0aa59957f1c4708cf
Fumo <o.redchenko90@gmail.com>
2021-03-24 14:34:52 +0200

    upload files

---
+++ js/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+    const form = $('form');
+    form.on('submit', formSend);
+
+    async function formSend (e) {
+        e.preventDefault();
+
+        let name = $('input[name=name]'),
+        name_text = name.val(),
+        email = $('input[name=email]'),
+        email_text = email.val();
+        
+        $.ajax({
+            url:'https://formspree.io/addr@example.com',
+            method:'POST',
+            data: {
+                name: name_text,
+                email: email_text
+            },
+            dataType: 'json'
+        }).done(function(){
+            alert('Данные отправлены')
+        }).fail(function(){
+            alert('Ошибка')
+        });
+    };
+
+
+    copies();
+    visitors();
+
+});
+
+let rand = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+let copies = () => {
+    let el = $(".slots .pin");
+    let left = +($(el).html());
+
+    left = left > 5 ? left - rand(1, 3) : left - rand(-2, 2);
+    if (left < 2) {
+        $(el).html(1);
+    } else {
+        $(el).html(left);
+    };
+
+    setTimeout('copies()', rand(9000, 13000));
+};
+
+let visitors = () => {
+    let el = $(".online .pin");
+    let left = +($(el).html());
+    let start = left - 20;
+    let end = left + 20;
+    if (start < 100) {
+        start = left;
+    };
+    if (end > 200) {
+        end = left;
+    };
+    $(el).html(rand(start, end));
+    setTimeout('visitors()', rand(3000, 13000));
+};
